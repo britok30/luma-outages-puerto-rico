25a688aa1ba472de1fa6201f5846531f1489529c
Kelvin Brito <britok30@gmail.com>
2022-09-22 17:33:01 -0400

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.apagonpuertorico.com/",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  // ...other options
+};
