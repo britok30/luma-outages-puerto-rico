50549316fcce2ba46be2eefa1e19be9c78c781c4
Kelvin Brito <britok30@gmail.com>
2022-09-22 16:40:42 -0400

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    i18n: {
-        locales: ['en', 'es'],
-        defaultLocale: 'en',
-    },
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+      },
+    ];
+  },
 };
