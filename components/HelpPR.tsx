a8d03c9f28213a4c8031b9709d632d789cb088e9
Kelvin Brito <britok30@gmail.com>
2022-09-22 15:50:35 -0400

    adding english translations

--- components/HelpPR.tsx
+++ components/HelpPR.tsx
@@ -1,128 +1,195 @@
-import React from 'react';
+import React from "react";
 
 const HelpPR = () => {
-    return (
-        <div className="mt-10 flex flex-col text-md justify-center items-center">
-            <h2 className="text-2xl md:text-4xl mb-2 text-blue-500">
-                Help Puerto Rico | Ayuda a Puerto Rico
-            </h2>
-            <div className="w-full md:w-2/3 lg:w-1/3 text-left">
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://www.instagram.com/p/Ciq7t-BsgI0/"
-                        className="font-bold underline text-red-400"
-                    >
-                        Brigada Solidaria
-                    </a>{' '}
-                    del Oeste recolecta donaciones de emergencia elementos
-                    esenciales que incluyen tabletas de purificación de agua,
-                    lámparas solares, agua filtros y botiquines de primeros
-                    auxilios.{' '}
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="http://www.puertoricancivicclub.org/"
-                        className="font-bold underline text-red-400"
-                    >
-                        Puerto Rican Civic Club{' '}
-                    </a>
-                    esta recaudando fondos para luces solares y generadores de
-                    gas.{' '}
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://en.tallersalud.com/"
-                        className="font-bold underline text-red-400"
-                    >
-                        Taller Salud
-                    </a>
-                    , una organización sin fines de lucro, dirigida por mujeres
-                    con sede en Loíza, Puerto Rico, está aceptando donaciones de
-                    alimentos no perecederos, recipientes desechables, galones
-                    de agua, pañales para adultos y bebés, y más. El grupo
-                    también acepta donaciones en efectivo a través de PayPal y
-                    algunos elementos esenciales de socorro en casos de
-                    desastre, incluidos filtros de agua y linternas solares.{' '}
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://www.comedoressocialespr.org/aportar"
-                        className="font-bold underline text-red-400"
-                    >
-                        Comedores Sociales de Puerto Rico{' '}
-                    </a>
-                    es un comedor comunitario con programas que combaten la
-                    inseguridad alimentaria en la isla.{' '}
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://www.globalgiving.org/projects/hurricane-fiona-relief-fund/"
-                        className="font-bold underline text-red-400"
-                    >
-                        Global Giving
-                    </a>
-                    , una organización sin fines de lucro que apoya y conecta a
-                    otras organizaciones sin fines de lucro con donantes, lanzó
-                    el "Fondo de ayuda para el huracán Fiona", con el objetivo
-                    de recaudar $ 1 millón para ayudar a los residentes de la
-                    isla y otras comunidades en todo el Caribe.{' '}
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://www.hispanicfederation.org/fionahelp/"
-                        className="font-bold underline text-red-400"
-                    >
-                        Hispanic Federation
-                    </a>
-                    , una organización sin fines de lucro, que brinda apoyo a
-                    las comunidades latinas en los EE. UU., dice que la
-                    organización ya está en Puerto Rico brindando servicios de
-                    ayuda de emergencia y suministros esenciales a las
-                    comunidades de toda la isla.
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://fundly.com/prxpr"
-                        className="font-bold underline text-red-400"
-                    >
-                        PRxPR Relief and Rebuild Fund
-                    </a>
-                    , lanzado a raíz del huracán María por líderes empresariales
-                    puertorriqueños en los EE. UU., trabaja con organizaciones
-                    locales en toda la isla para ayudar a reconstruir la
-                    infraestructura y brindar ayuda en casos de desastre.
-                </p>
-                <p className="mb-4 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
-                    <a
-                        target="_blank"
-                        rel="no_referrer"
-                        href="https://www.tpmgcorp.org/"
-                        className="font-bold underline text-red-400"
-                    >
-                        Techos Pa' Mi Gente
-                    </a>{' '}
-                    es una organización sin fines de lucro que comenzó en 2017
-                    después del huracán María. La organización se enfoca en
-                    proporcionar reconstrucción de viviendas a comunidades
-                    devastadas.
-                </p>
-            </div>
+  return (
+    <div className="mt-10 flex flex-col text-md justify-center items-center">
+      <h2 className="text-2xl md:text-4xl mb-2 text-blue-500">
+        Help Puerto Rico | Ayuda a Puerto Rico
+      </h2>
+      <div className="w-full md:w-2/3 lg:w-1/3 text-left">
+        <div className="mb-4">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://www.instagram.com/p/Ciq7t-BsgI0/"
+              className="font-bold underline text-red-400"
+            >
+              Brigada Solidaria del Oeste
+            </a>{" "}
+            recolecta donaciones de emergencia elementos esenciales que incluyen
+            tabletas de purificación de agua, lámparas solares, agua filtros y
+            botiquines de primeros auxilios.{" "}
+          </p>
+          <p className="mt-1 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Brigada Solidaria del Oeste collects emergency donation essentials
+            including water purification tablets, solar lamps, water filters and
+            first aid kits.
+          </p>
         </div>
-    );
+
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="http://www.puertoricancivicclub.org/"
+              className="font-bold underline text-red-400"
+            >
+              Puerto Rican Civic Club{" "}
+            </a>
+            esta recaudando fondos para luces solares y generadores de gas.{" "}
+          </p>
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Puerto Rican Civic Club is raising funds for solar lights and gas
+            generators.
+          </p>
+        </div>
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://en.tallersalud.com/"
+              className="font-bold underline text-red-400"
+            >
+              Taller Salud
+            </a>
+            , una organización sin fines de lucro, dirigida por mujeres con sede
+            en Loíza, Puerto Rico, está aceptando donaciones de alimentos no
+            perecederos, recipientes desechables, galones de agua, pañales para
+            adultos y bebés, y más. El grupo también acepta donaciones en
+            efectivo a través de PayPal y algunos elementos esenciales de
+            socorro en casos de desastre, incluidos filtros de agua y linternas
+            solares.{" "}
+          </p>
+
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Taller Salud, a women-led, nonprofit organization based in Loíza,
+            Puerto Rico, is accepting donations of non-perishable food,
+            disposable containers, gallons of water, adult and baby diapers, and
+            more. The group also accepts cash donations through PayPal and some
+            disaster relief essentials, including water filters and solar
+            lanterns.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://www.comedoressocialespr.org/aportar"
+              className="font-bold underline text-red-400"
+            >
+              Comedores Sociales de Puerto Rico{" "}
+            </a>
+            es un comedor comunitario con programas que combaten la inseguridad
+            alimentaria en la isla.{" "}
+          </p>
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Comedores Sociales de Puerto Rico is a community kitchen with
+            programs that combat food insecurity on the island.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://www.globalgiving.org/projects/hurricane-fiona-relief-fund/"
+              className="font-bold underline text-red-400"
+            >
+              Global Giving
+            </a>
+            , una organización sin fines de lucro que apoya y conecta a otras
+            organizaciones sin fines de lucro con donantes, lanzó el "Fondo de
+            ayuda para el huracán Fiona", con el objetivo de recaudar $ 1 millón
+            para ayudar a los residentes de la isla y otras comunidades en todo
+            el Caribe.{" "}
+          </p>
+
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Global Giving a nonprofit organization that supports and connects
+            other nonprofits with donors, launched the "Hurricane Fiona Relief
+            Fund," with the goal of raising $1 million to help residents of the
+            island and other communities throughout the Caribbean
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://www.hispanicfederation.org/fionahelp/"
+              className="font-bold underline text-red-400"
+            >
+              Hispanic Federation
+            </a>
+            , una organización sin fines de lucro, que brinda apoyo a las
+            comunidades latinas en los EE. UU., dice que la organización ya está
+            en Puerto Rico brindando servicios de ayuda de emergencia y
+            suministros esenciales a las comunidades de toda la isla.
+          </p>
+
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Hispanic Federation is a non-profit organization that supports
+            Latino communities in the US, says the organization is already in
+            Puerto Rico providing emergency aid services and essential supplies
+            to communities across the island.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://fundly.com/prxpr"
+              className="font-bold underline text-red-400"
+            >
+              PRxPR Relief and Rebuild Fund
+            </a>
+            , lanzado a raíz del huracán María por líderes empresariales
+            puertorriqueños en los EE. UU., trabaja con organizaciones locales
+            en toda la isla para ayudar a reconstruir la infraestructura y
+            brindar ayuda en casos de desastre.
+          </p>
+
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            PRxPR Relief and Rebuild Fund, launched in the wake of Hurricane
+            Maria by Puerto Rican business leaders in the US, it works with
+            local organizations across the island to help rebuild infrastructure
+            and provide disaster relief.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <p className="font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            <a
+              target="_blank"
+              rel="no_referrer"
+              href="https://www.tpmgcorp.org/"
+              className="font-bold underline text-red-400"
+            >
+              Techos Pa' Mi Gente
+            </a>{" "}
+            es una organización sin fines de lucro que comenzó en 2017 después
+            del huracán María. La organización se enfoca en proporcionar
+            reconstrucción de viviendas a comunidades devastadas.
+          </p>
+
+          <p className="mt-2 font-light text-gray-400 hover:text-white transition duration-200 ease-in-out">
+            Techos Pa' Mi Gente is a non-profit organization that started in
+            2017 after of Hurricane Maria. The organization focuses on providing
+            rebuilding homes to devastated communities.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HelpPR;
