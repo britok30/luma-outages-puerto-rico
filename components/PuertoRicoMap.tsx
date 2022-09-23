3ab6a19dce6c4c3bf9d4b61e326faacda3bee5c7
Kelvin Brito <britok30@gmail.com>
2022-09-23 04:37:33 -0400

    mapbox integration

---
+++ components/PuertoRicoMap.tsx
@@ -0,0 +1,128 @@
+import { Feature } from "geojson";
+import React, { useCallback, useState, useMemo, useEffect } from "react";
+import Map, { Layer, Source } from "react-map-gl";
+import RegionsJSON from "../pages/api/puerto-rico.json";
+import { Regions } from "../types";
+import { useDebouncedCallback } from "use-debounce";
+import { useWindowSize } from "../hooks";
+
+interface HoverInfo {
+  x: number;
+  y: number;
+  feature: Feature;
+}
+
+export const PuertoRicoMap = ({ regions }: { regions: Regions[] }) => {
+  const [hoverInfo, setHoverInfo] = useState<HoverInfo | null>(null);
+  const onHover = useCallback((event: mapboxgl.MapLayerMouseEvent) => {
+    event.originalEvent.preventDefault();
+    event.originalEvent.stopPropagation();
+    const {
+      features,
+      point: { x, y },
+    } = event;
+    const hoveredFeature = features && features[0];
+
+    if (hoveredFeature) {
+      setHoverInfo({ feature: hoveredFeature, x, y });
+    }
+  }, []);
+
+  const updateJSON = (
+    featureCollection: GeoJSON.FeatureCollection<GeoJSON.Geometry>
+  ): GeoJSON.FeatureCollection<GeoJSON.Geometry> => {
+    const { features } = featureCollection;
+    return {
+      type: "FeatureCollection",
+      features: features.map((f, index) => {
+        const properties = {
+          ...f.properties,
+          totalClients: regions[index].totalClients,
+          totalClientsWithoutService: regions[index].totalClientsWithoutService,
+          percentageClientsWithoutService:
+            regions[index].totalClientsWithoutService,
+        };
+
+        return { ...f, properties };
+      }),
+    };
+  };
+
+  const data = useMemo(() => {
+    return updateJSON(
+      RegionsJSON as GeoJSON.FeatureCollection<GeoJSON.Geometry>
+    );
+  }, [regions]);
+
+  return (
+    <div className="w-full md:w-2/3 h-[400px] md:h-[600px] mb-20 relative">
+      <div className="mb-5">
+        <h2 className="text-2xl md:text-4xl mb-2 text-blue-500">
+          Puerto Rico Map - Region Outages
+        </h2>
+        <p className="text-xs md:text-sm">
+          Hover (click on mobile) on region to see stats
+        </p>
+      </div>
+      <Map
+        initialViewState={{
+          longitude: -66.664513,
+          latitude: 18.200178,
+          zoom: 8,
+        }}
+        scrollZoom={false}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        mapboxAccessToken="pk.eyJ1IjoiYnJpdG9rMzAiLCJhIjoiY2w4ZG1wM3ZiMDB2eTNvbGV2eTBlb2YzdSJ9.FcY2LS-TSORQRUo7iowoDw"
+        onMouseMove={onHover}
+        interactiveLayerIds={["fillLayer"]}
+      >
+        <Source id="puerto-rico" type="geojson" data={data}>
+          <Layer
+            id="fillLayer"
+            type="fill"
+            source="puerto-rico"
+            paint={{
+              "fill-color": {
+                property: "totalClientsWithoutService",
+                stops: [
+                  [150000, "#BA324F"],
+                  [170000, "#4BA3C3"],
+                  [190000, "#D62839"],
+                  [210000, "#175676"],
+                ],
+              },
+              "fill-opacity": 0.6,
+            }}
+          />
+          <Layer
+            id="lineLayer"
+            type="line"
+            source="puerto-rico"
+            paint={{
+              "line-color": "#000",
+              "line-width": 1,
+            }}
+          />
+          {hoverInfo && (
+            <div
+              className="absolute bg-black top-0 left-[50%] border-white text-white p-2 rounded-lg text-left"
+              style={{
+                transform: "translate(-50%,10%)",
+                backfaceVisibility: "hidden",
+              }}
+            >
+              <div>Region: {hoverInfo.feature.properties?.name}</div>
+              <div>
+                Total Clients: {hoverInfo.feature.properties?.totalClients}
+              </div>
+              <div>
+                Clients Without Service:{" "}
+                {hoverInfo.feature.properties?.totalClientsWithoutService}
+              </div>
+            </div>
+          )}
+        </Source>
+      </Map>
+    </div>
+  );
+};
--- hooks.ts
+++ hooks.ts
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import Regions from "./pages/api/puerto-rico.json";
 
 export const useWindowSize = () => {
   // Initialize state with undefined width/height so server and client renders match
@@ -28,4 +30,63 @@ export const useWindowSize = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
+};
+
+export const useMapBox = () => {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYnJpdG9rMzAiLCJhIjoiY2w4ZG1wM3ZiMDB2eTNvbGV2eTBlb2YzdSJ9.FcY2LS-TSORQRUo7iowoDw";
+
+  const mapContainer = useRef<any>(null);
+  const map = useRef<any>(null);
+  const [lng, setLng] = useState(-66.6);
+  const [lat, setLat] = useState(18.2);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://sprites/mapbox/streets-v8",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    // disable map zoom when using scroll
+    map.current.scrollZoom.disable();
+
+    map.current.on("load", () => {
+      // Add a data source containing GeoJSON data.
+      map.current.addSource("puerto-rico", {
+        type: "geojson",
+        data: Regions,
+      });
+
+      // Add a new layer to visualize the polygon.
+      map.current.addLayer({
+        id: "puerto-rico",
+        type: "fill",
+        source: "puerto-rico", // reference the data source
+        layout: {},
+        paint: {
+          "fill-color": "#0080ff", // blue color fill
+          "fill-opacity": 0.5,
+        },
+      });
+
+      // Add a black outline around the polygon.
+      map.current.addLayer({
+        id: "puerto-rico",
+        type: "line",
+        source: "puerto-rico",
+        layout: {},
+        paint: {
+          "line-color": "#000",
+          "line-width": 3,
+        },
+      });
+    });
+  });
+
+  return mapContainer;
 };
